1d14e31173f322c3a9c058e1a58098266f223758
Krzysztof Ryk <kryk@mbp-krzysztof.home>
2014-07-10 23:40:02 +0200

    Initial commit

---
+++ src/test/scala/BlogSpec.scala
@@ -0,0 +1,11 @@
+import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.selenium.HtmlUnit
+
+class BlogSpec extends FunSuite with Matchers with HtmlUnit {
+
+  val host = "http://www.onet.pl"
+  test("The home page should have a title which contains onet") {
+    go to (host)
+    assert(pageTitle contains ("Onet"));
+  }
+}
